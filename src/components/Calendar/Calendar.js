4c4fb688eb49ca15ee1ff832ea4920cff68b8e61
Valery <lekalera777@gmail.com>
2020-10-11 20:02:59 +0300

    code refactoring

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -39,40 +39,30 @@ const Calendar = ({ onCellClick }) => {
             Object.values(calendarRows).map(cols => {
               return <tr key={cols[0].date}>
                 {cols.map(col => (
-                  JSON.stringify(col.date) === JSON.stringify(todayDateFormatted)
-                    ? <td key={col.date} className={`${col.classes} day today`}
-
-                    >{col.value}
-                      <span className="create-event" onClick={() => {
-                        dateClickHandler(col.date);
-                      }}>+</span>
-                    </td>
-                    : <td key={col.date} className={`${col.classes} day`}
-                    >
-                      {col.value}
-                      <span className="create-event" onClick={() => {
-                        dateClickHandler(col.date);
-                        console.log(col.date.toLocaleDateString())
-                        console.log(saveData)
-                      }}>+</span>
-
-                      {saveData.map((el) => {
-                        if (el.day === col.date.toLocaleDateString()) {
-                          return (
-                            <div className='event'>
-                              <label htmlFor="status">
-                                <input type="checkbox" id="status" />
-                                <span className="event__time-start">{el.timeStart}</span>
-                                <span className='event__title'>{el.title}</span>
-                              </label>
-                            </div>
-                          )
-                        }
-                      }).sort(function (a, b) {
-                        return new Date(a.dateStart + " " + a.timeStart) - new Date(b.dateStart + " " + b.timeStart);
-                      })}
-                    </td>
-                ))}
+                  <td key={col.date} className={JSON.stringify(col.date) === JSON.stringify(todayDateFormatted) ? `${col.classes} day today` : `${col.classes} day`}
+                  >{col.value}
+                    <span className="create-event" onClick={() => {
+                      dateClickHandler(col.date);
+                      console.log(col.date.toLocaleDateString())
+                      console.log(saveData)
+                    }}>+</span>
+                    {saveData.map((el) => {
+                      if (el.day === col.date.toLocaleDateString()) {
+                        return (
+                          <div className='event'>
+                            <label htmlFor="status">
+                              <input type="checkbox" id="status" />
+                              <span className="event__time-start">{el.timeStart}</span>
+                              <span className='event__title'>{el.title}</span>
+                            </label>
+                          </div>
+                        )
+                      }
+                    }).sort(function (a, b) {
+                      return new Date(a.dateStart + " " + a.timeStart) - new Date(b.dateStart + " " + b.timeStart);
+                    })}
+                  </td>)
+                )}
               </tr>
             })
           }
