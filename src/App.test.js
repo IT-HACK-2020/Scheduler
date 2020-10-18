cc69af5aa07e134189a6b00d1f20f5e3688577f4
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-10-18 11:56:28 +0300

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,14 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
 const playwright = require('playwright');
 jest.setTimeout(120000);
 
-const pathToScheduler = "http://localhost:3000/";
+const pathToScheduler = "https://hackathonlogin-95995.firebaseapp.com";
 let browser;
 let context;
 let page;
 
 
 beforeAll(async () => {
-    browser = await playwright.firefox.launch({headless: false, slowMo: 1000});
+    browser = await playwright.webkit.launch({headless: false, slowMo: 1000});
 })
 
 beforeEach(async () => {
@@ -27,18 +24,50 @@ afterAll(async () => {
     await browser.close();
 })
 
-
 describe ("Personal scheduling UI tests", () =>{
-  test("Open scheduler", async () => {
+  test("0. Open scheduler", async () => {
     await page.goto(pathToScheduler);
     await page.waitForSelector("#root");
-
   })
+  
+  test("3.1 Scroll of months to the left", async () => {
+    await page.goto(pathToScheduler);
+    await page.waitForSelector(".Arrow");
+    await page.click(".Arrow");
+    await page.waitForSelector(".month");
+    const nameOfPrevMonth = await page.$eval(".month", el => el.textContent);
+    expect(nameOfPrevMonth).toEqual("Сентябрь - 2020");
+  })
+  test("3.2 Scroll of months to the right", async () => {
+    await page.goto(pathToScheduler);
+    await page.waitForSelector(".arrow-next");
+    await page.click(".arrow-next");
+    const nameOfNextMonth = await page.$eval(".month", el => el.textContent);
+    expect(nameOfNextMonth).toEqual("Ноябрь - 2020");
+  })
+  test("3.3 Scroll of months by Today button", async () => {
+    await page.goto(pathToScheduler);
+    await page.waitForSelector(".Arrow");
+    // Store value of current month
+    const nameOfCurrentMonth = await page.$eval(".month", el => el.textContent);
+    await page.click(".Arrow");
+    const firstOfNewMonth = await page.$eval(".month", el => el.textContent);
+    await page.click(".Arrow");
+    await page.waitForSelector(".header__today");
+    await page.click(".header__today");
+    const nameOfNewMonth = await page.$eval(".month", el => el.textContent);
+    expect(nameOfNewMonth).toEqual(nameOfCurrentMonth);
+  })
+
+  test.skip("7.1 Change event status to done", async () => {
+    await page.goto(pathToScheduler);
+    //Create Event
+    await page.waitForSelector("div.event :nth-child(2) > input");
+    const oldNumberEvents = await page.$eval("span.done-event", el => el.textContent);
+    await page.click("div.event :nth-child(2) > input");
+    const newNumberEvents = await page.$eval("span.done-current", el => el.textContent);
+    expect(Number(newNumberEvents)).toEqual(Number(oldNumberEvents)-1);
+  })
+
+
 })
-
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
